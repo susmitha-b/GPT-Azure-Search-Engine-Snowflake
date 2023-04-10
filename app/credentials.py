19a948a8768c76ee343516ec21c8f263e74b4913
susmitha <bysani.venka@hcl.com>
2023-04-10 10:16:11 +0000

    Snowflake changes

--- app/credentials.py
+++ app/credentials.py
@@ -1,18 +1,18 @@
 
 # We recommend leaving these two variables as below. The DATASOURCE_CONNECTION_STRING contains 9800 PDFs from Arxiv Computer Science publications. Change it if you want to use your own data.
-DATASOURCE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=demodatasetsp;AccountKey=K9rLf3hFmKN4oZunp3xIIqjlxkyqtOfcdJQgDk65cHSYAY29rvoTx2S1x2t+mJ5tv+3LRN/FES3Q+AStuqGYIg==;EndpointSuffix=core.windows.net"
-DATASOURCE_SAS_TOKEN = "?sv=2021-12-02&ss=b&srt=sco&sp=rltfx&se=2024-04-05T09:10:04Z&st=2023-04-05T01:10:04Z&spr=https&sig=Rm52BKJs%2BziCnKSmCHdWsQqVki4FsMuiH25MsZAN7Z8%3D"
+DATASOURCE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=depprocureformstorage;AccountKey=Ze0i+1gPZIw5NRNV/r9C+/comhFAX+jOCSNVzN7jSMwmQqCKf/Aw3oq/Qi3sUkLDLcFviEXNawbl+AStqOf7Hg==;EndpointSuffix=core.windows.net"
+DATASOURCE_SAS_TOKEN = "?sv=2021-12-02&ss=bfqt&srt=c&sp=rwdlacupiytfx&se=2023-04-10T17:34:30Z&st=2023-04-10T09:34:30Z&spr=https&sig=ctQTfHnfwJ6Vtn7YNJ9oGjHgg5I8q%2B%2FNbprDs93Ra4E%3D"
 AZURE_SEARCH_API_VERSION = '2021-04-30-Preview'
 AZURE_OPENAI_API_VERSION = "2023-03-15-preview"
 
 # Change these below with your own services credentials
 
-AZURE_SEARCH_ENDPOINT = "https://azure-cog-search-cstevuxaqrxcm.search.windows.net"
-AZURE_SEARCH_KEY = "fET2lKwQ4akc6sFLF1YQ376eNRyrpJmspxSqMiB7eZAzSeBJZG43"
-COG_SERVICES_NAME = "cognitive-service-cstevuxaqrxcm"
-COG_SERVICES_KEY = "3cffb8c5ba1249df88399968a499ad54"
-AZURE_OPENAI_ENDPOINT = "https://open-ai-pinternal.openai.azure.com/"
-AZURE_OPENAI_KEY = "b2dc4d69945240a993288400d3b18faf"
+AZURE_SEARCH_ENDPOINT = "https://azure-cog-search-g2ysbgn5zpihq.search.windows.net"
+AZURE_SEARCH_KEY = "Uhv7jafKF7319Ms95bt3efeGE7e0FLGgR0eKltHvNcAzSeDXlfN5"
+COG_SERVICES_NAME = "cognitive-service-g2ysbgn5zpihq"
+COG_SERVICES_KEY = "4c76a8adf0ab431a8ecc1e78dcfa459d"
+AZURE_OPENAI_ENDPOINT = "https://openaipractice.openai.azure.com/"
+AZURE_OPENAI_KEY = "7b90a78f0e074117826c4a9f8205845e"
 
 # AZURE_SEARCH_ENDPOINT = "Enter your Azure Cognitive Search Endpoint ..."
 # AZURE_SEARCH_KEY = "Enter your Azure Cognitive Search Primary Key ..."
@@ -20,8 +20,3 @@ AZURE_OPENAI_KEY = "b2dc4d69945240a993288400d3b18faf"
 # COG_SERVICES_KEY = "Enter your Azure Cognitive services multi-service Key ..."
 # AZURE_OPENAI_ENDPOINT = "Enter your Azure OpenAI Endpoint ..."
 # AZURE_OPENAI_KEY = "Enter your Azure OpenAI Key ..."
-
-
-
-
-
